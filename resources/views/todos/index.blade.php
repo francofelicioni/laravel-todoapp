81b9075c97a8e55d89d6b35f8549b9fcadfed4d7
Fran Felicioni <fran.felicioni@gmail.com>
2023-05-19 09:31:19 +0200

    Initialized project

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+
+@section('content')
+<div class="container w-25 border p-4 mt-4">
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Task Title</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">Create a new task</div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+@endsection
