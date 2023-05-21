81f1c62530c921b7205cfe170f21687c55e99b7e
Fran Felicioni <fran.felicioni@gmail.com>
2023-05-21 19:06:13 +0200

    First insert created in controller

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodosController extends Controller
 {
-    //
+    /**
+     * index > to show all todos
+     * store > to save a todo
+     * update > to update a todo
+     * destroy > to delete a a todo
+     * edit > to show edition form
+     **/
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required | min:3'
+        ]);
+
+        $todo = new Todo;
+        $todo->title = $request->title;
+        $todo->save();
+
+        return redirect()->route('todos')->with('success', 'Task successfully created');
+    }
 }
