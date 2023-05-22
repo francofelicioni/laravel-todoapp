db87510250b745d6755e4b2c74e8391134143cc0
Fran Felicioni <fran.felicioni@gmail.com>
2023-05-22 12:19:10 +0200

    Adding new validations for new and updated tasks

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,13 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My tasks</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link href="../css/app.css" rel="stylesheet">
+    <!-- <link href="{{asset('../css/app.css')}}" rel="stylesheet"> -->
 
     <style>
         body {
             padding: 0;
             margin: 0;
             font-family: sans-serif;
+            /* background-color: red; */
         }
 
         .color-container {
@@ -33,17 +34,15 @@
 
 
 <body>
-
-
     <nav class="navbar navbar-expand-lg bg-dark ">
         <div class="container-fluid">
-            <a class="navbar-brand text-light" href="#">TASKS DASHBOARD</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <p class="navbar-brand text-light" href="#">TASKS DASHBOARD</p>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="collapse navbar-collapse" id="navbarNav">
                 <div class="navbar-nav">
-                    <a class="nav-link text-light" aria-current="page" href="#">Tasks </a>
+                    <a class="nav-link text-light" href="{{ route ('todos') }}">Tasks </a>
                     <a class="nav-link text-light" href="#">Categories</a>
                 </div>
             </div>
